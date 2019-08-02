898e5ed5d9ddba879d86abe08d769a1abea5a7de
myevan <myevan@outlook.com>
2019-08-03 08:41:54 +0900

    test: base.pattern, asset_xlsx added

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 python -m engine.base.data
+python -m engine.base.pattern
 python -m engine.core.data
 python -m engine.core.caches
 python -m engine.core.assets.asset_csv
+python -m engine.core.assets.asset_xlsx
 python -m engine.protocol.types
