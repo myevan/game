b1f3e248b3ece77b83e712eeee24ebe217fa0af6
myevan <myevan@outlook.com>
2019-08-03 08:13:24 +0900

    Error.__str__&category added

--- engine/base/error.py
+++ engine/base/error.py
@@ -2,3 +2,10 @@ class Error(Exception):
     def __init__(self, name):
         Exception.__init__(self, name)
         self.name = name
+
+    def __str__(self):
+        return f"{self.__class__.__name__}<{self.name}>"
+
+    @property
+    def category(self):
+        return f"{self.__class__.__name__}<{self.name}>"
