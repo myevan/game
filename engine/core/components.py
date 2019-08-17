e657f9daecc27d71d49197037eac809ea9c18880
myevan <myevan@outlook.com>
2019-08-17 15:41:47 +0900

    core.components: CellGrid added

--- engine/core/components.py
+++ engine/core/components.py
@@ -6,8 +6,28 @@ from .primitives import Position, Rotation
 
 class ComponentNum(Enum):
     Cell = 1
-    Transform = 2
-    Identity = 3
+    CellGrid = 2
+    Transform = 3
+    Identity = 4
+    Custom = 10
+
+class CellGrid(Component):
+    def __init__(self, *args, **kwargs):
+        Component.__init__(self, *args, **kwargs)
+        self.eids = []
+
+    def create(self, width, height):
+        self.width = width
+        self.height = height
+        self.eids = [0] * height * width
+
+    def set(self, row, col, eid):
+        offset = self.width * row + col
+        self.eids[offset] = eid
+
+    def get(self, row, col):
+        offset = self.width * row + col
+        return self.eids[offset]
 
 class Cell(Component):
     row = Integer()
