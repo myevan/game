0f0499f6b67dee325bc709283370a20bc5b2d5aa
myevan <myevan@outlook.com>
2019-08-17 14:20:03 +0900

    ex03_fight fitted to ex02_move

--- examples/rpg/ex03_fight.py
+++ examples/rpg/ex03_fight.py
@@ -11,15 +11,22 @@ comp_factory.register(CN.Cell, Cell)
 from engine.core.ecs import World
 world = World(100)
 
-from engine.core.systems.system_tcod import TCView
-tc_view = TCView(world, "rpg")
-
 from engine.core.ecs import SystemManager
 sys_mgr = SystemManager(world)
 
+from engine.core.systems.system_tcod import TCView
+tc_view = TCView(world, "rpg")
+sys_mgr.add(tc_view)
+
 from engine.core.events import EventNum
 world.bind_event(EventNum.App, tc_view)
-sys_mgr.add(tc_view)
+
+eid = world.spawn([CN.Cell])
+ent = world.get_entity(eid)
+cell = ent.get(CN.Cell)
+cell.row = 30
+cell.col = 40
+cell.val = '@'
 
 from engine.core.ecs import System, EventHandler
 class EXPlayer(System, EventHandler):
@@ -48,12 +55,6 @@ class EXPlayer(System, EventHandler):
             elif evt.key_num == KeyNum.Right:
                 self.cell.col += 1
 
-eid = world.spawn([CN.Cell])
-ent = world.get_entity(eid)
-cell = ent.get(CN.Cell)
-cell.row = 30
-cell.col = 40
-cell.val = '@'
 ex_player = EXPlayer(world, eid)
 world.bind_event(EventNum.Key, ex_player)
 sys_mgr.add(ex_player)
